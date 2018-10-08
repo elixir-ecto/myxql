a6bfed4aeaa37d75b708c6ff1df050fc63fe0d90
Benjamin Schultzer <Schultzer@users.noreply.github.com>
2018-10-08 01:18:28 -0700

    Leverage binary pattern matching (#1)

--- lib/myxql/utils.ex
+++ lib/myxql/utils.ex
@@ -3,6 +3,7 @@ defmodule MyXQL.Utils do
   use Bitwise
 
   # https://dev.mysql.com/doc/internals/en/secure-password-authentication.html
+  @spec mysql_native_password(binary(), binary()) :: binary()
   def mysql_native_password(password, auth_plugin_data)
       when is_binary(password) and is_binary(auth_plugin_data) do
     password_sha1 = :crypto.hash(:sha, password)
@@ -13,10 +14,5 @@ defmodule MyXQL.Utils do
     )
   end
 
-  defp bxor_binary(b1, b2) do
-    for {e1, e2} <- List.zip([:erlang.binary_to_list(b1), :erlang.binary_to_list(b2)]) do
-      e1 ^^^ e2
-    end
-    |> :erlang.list_to_binary()
-  end
+  defp bxor_binary(<<l::160>>, <<r::160>>), do: <<(l ^^^ r)::160>>
 end
