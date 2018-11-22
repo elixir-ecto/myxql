2659112e35fd66513c4ead9d6623595063f02baa
Cheng Zhe <chengzhe02@qq.com>
2018-11-22 16:38:10 +0800

    Fix encode_length_encoded_integer/1 (#12)

--- lib/myxql/types.ex
+++ lib/myxql/types.ex
@@ -28,14 +28,14 @@ defmodule MyXQL.Types do
   def take_length_encoded_integer(<<0xFE, int::int(8), rest::binary>>), do: {int, rest}
 
   # https://dev.mysql.com/doc/internals/en/string.html#packet-Protocol::LengthEncodedString
-  def encode_length_encode_integer(int) when int < 251, do: int
+  def encode_length_encoded_integer(int) when int < 251, do: <<int>>
   def encode_length_encoded_integer(int) when int < 0xFFFF, do: <<0xFC, int::int(2)>>
   def encode_length_encoded_integer(int) when int < 0xFFFFFF, do: <<0xFD, int::int(3)>>
 
   def encode_length_encoded_integer(int) when int < 0xFFFFFFFFFFFFFFFF,
     do: <<0xFE, int::int(8)>>
 
-  def encode_length_encode_string(binary) when is_binary(binary) do
+  def encode_length_encoded_string(binary) when is_binary(binary) do
     size = encode_length_encoded_integer(byte_size(binary))
     <<size::binary, binary::binary>>
   end
@@ -252,7 +252,7 @@ defmodule MyXQL.Types do
   def encode_binary_value(%NaiveDateTime{} = datetime), do: encode_binary_datetime(datetime)
 
   def encode_binary_value(binary) when is_binary(binary) do
-    {@mysql_type_var_string, encode_length_encode_string(binary)}
+    {@mysql_type_var_string, encode_length_encoded_string(binary)}
   end
 
   def encode_binary_value(true) do
