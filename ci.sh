0f867cdf9f13ea885961199d4a1fc0bbeb5f2e92
Wojtek Mach <wojtekmach@users.noreply.github.com>
2018-12-22 18:11:32 +0100

    Run compile-time checks on CI (#16)

---
+++ ci.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+set -x
+
+echo $CHECKS_ONLY
+if [ -n "$CHECKS_ONLY" ]; then
+  mix compile --warnings-as-errors
+  mix format --check-formatted
+else
+  docker pull $DB || true
+  sudo chmod 777 -R /var/run/mysqld
+  docker run --name mysql -p 3306:3306 --volume /var/run/mysqld:/var/run/mysqld -e MYSQL_ALLOW_EMPTY_PASSWORD=1 -d $DB
+  mysql --version
+  until mysql -u root -e "SELECT @@version;"; do sleep 1; done
+  mix test
+fi
