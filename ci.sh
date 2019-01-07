a29dee81cdcdb3e6af0b60497e0ea0ad5ebb5ecc
Wojtek Mach <wojtek@wojtekmach.pl>
2019-01-07 15:14:03 +0100

    Simplify CI setup

--- ci.sh
+++ ci.sh
@@ -2,23 +2,18 @@
 set -e
 set -x
 
-if [ -n "$CHECKS_ONLY" ]; then
-  mix compile --warnings-as-errors
-  mix format --check-formatted
-else
-  : "${DB:=mysql:8.0}"
-  : "${MYSQL_TCP_PORT:=3306}"
-  : "${MYSQL_SOCKET_DIR:=/tmp/mysqld}"
-  name="myxql-ci"
+: "${DB:=mysql:8.0}"
+: "${MYSQL_TCP_PORT:=3306}"
+: "${MYSQL_SOCKET_DIR:=/tmp/mysqld}"
+name="myxql-ci"
 
-  mkdir -p $MYSQL_SOCKET_DIR
-  sudo chmod 777 $MYSQL_SOCKET_DIR
+mkdir -p $MYSQL_SOCKET_DIR
+sudo chmod 777 $MYSQL_SOCKET_DIR
 
-  docker pull $DB || true
-  docker rm --force $name || true
-  docker run --name $name -p $MYSQL_TCP_PORT:3306 --volume $MYSQL_SOCKET_DIR:/var/run/mysqld -e MYSQL_ALLOW_EMPTY_PASSWORD=1 -d $DB
-  mysql --version
-  until mysql -u root --protocol=tcp -e "SELECT @@version;"; do sleep 1; done
-  mix test $1
-  docker rm --force $name || true
-fi
+docker pull $DB || true
+docker rm --force $name || true
+docker run --name $name -p $MYSQL_TCP_PORT:3306 --volume $MYSQL_SOCKET_DIR:/var/run/mysqld -e MYSQL_ALLOW_EMPTY_PASSWORD=1 -d $DB
+mysql --version
+until mysql -u root --protocol=tcp -e "SELECT @@version;"; do sleep 1; done
+mix test $1
+docker rm --force $name || true
