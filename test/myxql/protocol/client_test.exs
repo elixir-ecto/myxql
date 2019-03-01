567287303f26c46378fc2f74c9f5b930c4c4b4f3
Wojtek Mach <wojtek@wojtekmach.pl>
2019-03-01 15:17:23 +0100

    Fix build
    This seems broken on Elixir v1.7 but fixed in newer versions:
    
        assert record(...) = ...

--- test/myxql/protocol/client_test.exs
+++ test/myxql/protocol/client_test.exs
@@ -36,8 +36,8 @@ defmodule MyXQL.Protocol.ClientTest do
     refute :server_status_cursor_exists in list_status_flags(status_flags)
     assert :server_status_last_row_sent in list_status_flags(status_flags)
 
-    assert {:ok, err_packet(message: "The statement (1) has no open cursor.")} =
-             Client.com_stmt_fetch(statement_id, column_defs, 2, state)
+    {:ok, err_packet(message: "The statement (1) has no open cursor.")} =
+      Client.com_stmt_fetch(statement_id, column_defs, 2, state)
   end
 
   test "com_stmt_fetch stored procedure", %{state: state} do
