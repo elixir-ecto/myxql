6ec4f0f80a9336aa199cb42b390aa0ed41c6b2b6
Wojtek Mach <wojtek@wojtekmach.pl>
2019-03-13 11:54:54 +0100

    Optimize binary row decoding

--- lib/myxql/protocol/values.ex
+++ lib/myxql/protocol/values.ex
@@ -135,226 +135,6 @@ defmodule MyXQL.Protocol.Values do
 
   # Binary values
 
-  def decode_binary_row(payload, column_defs) do
-    size = div(length(column_defs) + 7 + 2, 8)
-    <<0x00, null_bitmap::uint(size), values::binary>> = payload
-    null_bitmap = null_bitmap >>> 2
-    decode_binary_row(values, null_bitmap, column_defs, [])
-  end
-
-  defp decode_binary_row(<<rest::binary>>, null_bitmap, [_column_def | tail], acc)
-       when (null_bitmap &&& 1) == 1 do
-    decode_binary_row(rest, null_bitmap >>> 1, tail, [nil | acc])
-  end
-
-  defp decode_binary_row(
-         <<value::int1, rest::binary>>,
-         null_bitmap,
-         [column_def(type: :mysql_type_tiny, unsigned?: false) | tail],
-         acc
-       ) do
-    decode_binary_row(rest, null_bitmap >>> 1, tail, [value | acc])
-  end
-
-  defp decode_binary_row(
-         <<value::uint1, rest::binary>>,
-         null_bitmap,
-         [column_def(type: :mysql_type_tiny, unsigned?: true) | tail],
-         acc
-       ) do
-    decode_binary_row(rest, null_bitmap >>> 1, tail, [value | acc])
-  end
-
-  defp decode_binary_row(
-         <<value::int2, rest::binary>>,
-         null_bitmap,
-         [column_def(type: type, unsigned?: false) | tail],
-         acc
-       )
-       when type in [:mysql_type_short, :mysql_type_year] do
-    decode_binary_row(rest, null_bitmap >>> 1, tail, [value | acc])
-  end
-
-  defp decode_binary_row(
-         <<value::uint2, rest::binary>>,
-         null_bitmap,
-         [column_def(type: type, unsigned?: true) | tail],
-         acc
-       )
-       when type in [:mysql_type_short, :mysql_type_year] do
-    decode_binary_row(rest, null_bitmap >>> 1, tail, [value | acc])
-  end
-
-  defp decode_binary_row(
-         <<value::int4, rest::binary>>,
-         null_bitmap,
-         [column_def(type: type, unsigned?: false) | tail],
-         acc
-       )
-       when type in [:mysql_type_long, :mysql_type_int24] do
-    decode_binary_row(rest, null_bitmap >>> 1, tail, [value | acc])
-  end
-
-  defp decode_binary_row(
-         <<value::uint4, rest::binary>>,
-         null_bitmap,
-         [column_def(type: type, unsigned?: true) | tail],
-         acc
-       )
-       when type in [:mysql_type_long, :mysql_type_int24] do
-    decode_binary_row(rest, null_bitmap >>> 1, tail, [value | acc])
-  end
-
-  defp decode_binary_row(
-         <<value::int8, rest::binary>>,
-         null_bitmap,
-         [column_def(type: :mysql_type_longlong, unsigned?: false) | tail],
-         acc
-       ) do
-    decode_binary_row(rest, null_bitmap >>> 1, tail, [value | acc])
-  end
-
-  defp decode_binary_row(
-         <<value::uint8, rest::binary>>,
-         null_bitmap,
-         [column_def(type: :mysql_type_longlong, unsigned?: true) | tail],
-         acc
-       ) do
-    decode_binary_row(rest, null_bitmap >>> 1, tail, [value | acc])
-  end
-
-  defp decode_binary_row(
-         <<value::little-signed-float-size(32), rest::binary>>,
-         null_bitmap,
-         [column_def(type: :mysql_type_float) | tail],
-         acc
-       ) do
-    decode_binary_row(rest, null_bitmap >>> 1, tail, [value | acc])
-  end
-
-  defp decode_binary_row(
-         <<value::little-signed-float-size(64), rest::binary>>,
-         null_bitmap,
-         [column_def(type: :mysql_type_double) | tail],
-         acc
-       ) do
-    decode_binary_row(rest, null_bitmap >>> 1, tail, [value | acc])
-  end
-
-  defp decode_binary_row(
-         <<data::binary>>,
-         null_bitmap,
-         [column_def(type: :mysql_type_newdecimal) | tail],
-         acc
-       ) do
-    {string, rest} = take_string_lenenc(data)
-    value = Decimal.new(string)
-    decode_binary_row(rest, null_bitmap >>> 1, tail, [value | acc])
-  end
-
-  defp decode_binary_row(<<data::binary>>, null_bitmap, [column_def(type: type) | tail], acc)
-       when type in [
-              :mysql_type_var_string,
-              :mysql_type_string,
-              :mysql_type_blob,
-              :mysql_type_long_blob
-              # :mysql_type_bit
-            ] do
-    {value, rest} = take_string_lenenc(data)
-    decode_binary_row(rest, null_bitmap >>> 1, tail, [value | acc])
-  end
-
-  defp decode_binary_row(
-         <<4, year::uint2, month::uint1, day::uint1, rest::binary>>,
-         null_bitmap,
-         [column_def(type: :mysql_type_date) | tail],
-         acc
-       ) do
-    {:ok, value} = Date.new(year, month, day)
-    decode_binary_row(rest, null_bitmap >>> 1, tail, [value | acc])
-  end
-
-  defp decode_binary_row(
-         <<8, 0::uint1, 0::uint4, hour::uint1, minute::uint1, second::uint1, rest::binary>>,
-         null_bitmap,
-         [column_def(type: :mysql_type_time) | tail],
-         acc
-       ) do
-    {:ok, value} = Time.new(hour, minute, second)
-    decode_binary_row(rest, null_bitmap >>> 1, tail, [value | acc])
-  end
-
-  defp decode_binary_row(
-         <<12, 0::uint1, 0::uint4, hour::uint1, minute::uint1, second::uint1, microsecond::uint4,
-           rest::binary>>,
-         null_bitmap,
-         [column_def(type: :mysql_type_time) | tail],
-         acc
-       ) do
-    {:ok, value} = Time.new(hour, minute, second, {microsecond, 6})
-    decode_binary_row(rest, null_bitmap >>> 1, tail, [value | acc])
-  end
-
-  defp decode_binary_row(
-         <<0, rest::binary>>,
-         null_bitmap,
-         [column_def(type: :mysql_type_time) | tail],
-         acc
-       ) do
-    value = ~T[00:00:00]
-    decode_binary_row(rest, null_bitmap >>> 1, tail, [value | acc])
-  end
-
-  defp decode_binary_row(
-         <<4, year::uint2, month::uint1, day::uint1, rest::binary>>,
-         null_bitmap,
-         [column_def(type: type) | tail],
-         acc
-       )
-       when type in [:mysql_type_datetime, :mysql_type_timestamp] do
-    value = new_datetime(type, year, month, day, 0, 0, 0, {0, 0})
-    decode_binary_row(rest, null_bitmap >>> 1, tail, [value | acc])
-  end
-
-  defp decode_binary_row(
-         <<7, year::uint2, month::uint1, day::uint1, hour::uint1, minute::uint1, second::uint1,
-           rest::binary>>,
-         null_bitmap,
-         [column_def(type: type) | tail],
-         acc
-       )
-       when type in [:mysql_type_datetime, :mysql_type_timestamp] do
-    value = new_datetime(type, year, month, day, hour, minute, second, {0, 0})
-    decode_binary_row(rest, null_bitmap >>> 1, tail, [value | acc])
-  end
-
-  defp decode_binary_row(
-         <<11, year::uint2, month::uint1, day::uint1, hour::uint1, minute::uint1, second::uint1,
-           microsecond::uint4, rest::binary>>,
-         null_bitmap,
-         [column_def(type: type) | tail],
-         acc
-       )
-       when type in [:mysql_type_datetime, :mysql_type_timestamp] do
-    value = new_datetime(type, year, month, day, hour, minute, second, {microsecond, 6})
-    decode_binary_row(rest, null_bitmap >>> 1, tail, [value | acc])
-  end
-
-  defp decode_binary_row(
-         <<data::binary>>,
-         null_bitmap,
-         [column_def(type: :mysql_type_json) | tail],
-         acc
-       ) do
-    {json, rest} = take_string_lenenc(data)
-    value = MyXQL.json_library().decode!(json)
-    decode_binary_row(rest, null_bitmap >>> 1, tail, [value | acc])
-  end
-
-  defp decode_binary_row("", _null_bitmap, [], acc) do
-    Enum.reverse(acc)
-  end
-
   def encode_binary_value(value)
       when is_integer(value) and value >= -1 <<< 63 and value < 1 <<< 64 do
     {:mysql_type_longlong, <<value::int8>>}
@@ -473,13 +253,253 @@ defmodule MyXQL.Protocol.Values do
     raise ArgumentError, "#{inspect(datetime)} is not in UTC"
   end
 
-  defp new_datetime(:mysql_type_datetime, year, month, day, hour, minute, second, microsecond) do
-    {:ok, naive_datetime} = NaiveDateTime.new(year, month, day, hour, minute, second, microsecond)
-    naive_datetime
+  def decode_binary_row(<<payload::bits>>, column_defs) do
+    size = div(length(column_defs) + 7 + 2, 8)
+    <<0x00, null_bitmap::uint(size), values::bits>> = payload
+    null_bitmap = null_bitmap >>> 2
+    types = Enum.map(column_defs, &column_def_to_type/1)
+    decode_binary_row(values, null_bitmap, types, [])
   end
 
-  defp new_datetime(:mysql_type_timestamp, year, month, day, hour, minute, second, microsecond) do
-    new_datetime(:mysql_type_datetime, year, month, day, hour, minute, second, microsecond)
-    |> DateTime.from_naive!("Etc/UTC")
+  defp column_def_to_type(column_def(type: type, unsigned?: unsigned?)),
+    do: column_def_to_type(type, unsigned?)
+
+  defp column_def_to_type(:mysql_type_tiny, true), do: :uint1
+  defp column_def_to_type(:mysql_type_tiny, false), do: :int1
+  defp column_def_to_type(:mysql_type_short, true), do: :uint2
+  defp column_def_to_type(:mysql_type_short, false), do: :int2
+  defp column_def_to_type(:mysql_type_long, true), do: :uint4
+  defp column_def_to_type(:mysql_type_long, false), do: :int4
+  defp column_def_to_type(:mysql_type_int24, true), do: :uint4
+  defp column_def_to_type(:mysql_type_int24, false), do: :int4
+  defp column_def_to_type(:mysql_type_longlong, true), do: :uint8
+  defp column_def_to_type(:mysql_type_longlong, false), do: :int8
+  defp column_def_to_type(:mysql_type_year, _), do: :uint2
+  defp column_def_to_type(:mysql_type_float, _), do: :float
+  defp column_def_to_type(:mysql_type_double, _), do: :double
+  defp column_def_to_type(:mysql_type_timestamp, _), do: :datetime
+  defp column_def_to_type(:mysql_type_date, _), do: :date
+  defp column_def_to_type(:mysql_type_time, _), do: :time
+  defp column_def_to_type(:mysql_type_datetime, _), do: :naive_datetime
+  defp column_def_to_type(:mysql_type_newdecimal, _), do: :decimal
+  defp column_def_to_type(:mysql_type_json, _), do: :json
+  defp column_def_to_type(:mysql_type_blob, _), do: :binary
+  defp column_def_to_type(:mysql_type_long_blob, _), do: :binary
+  defp column_def_to_type(:mysql_type_var_string, _), do: :binary
+  defp column_def_to_type(:mysql_type_string, _), do: :binary
+
+  defp decode_binary_row(<<rest::bits>>, null_bitmap, [_type | t], acc)
+       when (null_bitmap &&& 1) == 1 do
+    decode_binary_row(rest, null_bitmap >>> 1, t, [nil | acc])
   end
+
+  defp decode_binary_row(<<r::bits>>, null_bitmap, [:binary | t], acc),
+    do: decode_string_lenenc(r, null_bitmap, t, acc)
+
+  defp decode_binary_row(<<r::bits>>, null_bitmap, [:int1 | t], acc),
+    do: decode_int1(r, null_bitmap, t, acc)
+
+  defp decode_binary_row(<<r::bits>>, null_bitmap, [:uint1 | t], acc),
+    do: decode_uint1(r, null_bitmap, t, acc)
+
+  defp decode_binary_row(<<r::bits>>, null_bitmap, [:int2 | t], acc),
+    do: decode_int2(r, null_bitmap, t, acc)
+
+  defp decode_binary_row(<<r::bits>>, null_bitmap, [:uint2 | t], acc),
+    do: decode_uint2(r, null_bitmap, t, acc)
+
+  defp decode_binary_row(<<r::bits>>, null_bitmap, [:int4 | t], acc),
+    do: decode_int4(r, null_bitmap, t, acc)
+
+  defp decode_binary_row(<<r::bits>>, null_bitmap, [:uint4 | t], acc),
+    do: decode_uint4(r, null_bitmap, t, acc)
+
+  defp decode_binary_row(<<r::bits>>, null_bitmap, [:int8 | t], acc),
+    do: decode_int8(r, null_bitmap, t, acc)
+
+  defp decode_binary_row(<<r::bits>>, null_bitmap, [:uint8 | t], acc),
+    do: decode_uint8(r, null_bitmap, t, acc)
+
+  defp decode_binary_row(<<r::bits>>, null_bitmap, [:float | t], acc),
+    do: decode_float(r, null_bitmap, t, acc)
+
+  defp decode_binary_row(<<r::bits>>, null_bitmap, [:double | t], acc),
+    do: decode_double(r, null_bitmap, t, acc)
+
+  defp decode_binary_row(<<r::bits>>, null_bitmap, [:decimal | t], acc),
+    do: decode_decimal(r, null_bitmap, t, acc)
+
+  defp decode_binary_row(<<r::bits>>, null_bitmap, [:json | t], acc),
+    do: decode_json(r, null_bitmap, t, acc)
+
+  defp decode_binary_row(<<r::bits>>, null_bitmap, [:date | t], acc),
+    do: decode_date(r, null_bitmap, t, acc)
+
+  defp decode_binary_row(<<r::bits>>, null_bitmap, [:time | t], acc),
+    do: decode_time(r, null_bitmap, t, acc)
+
+  defp decode_binary_row(<<r::bits>>, null_bitmap, [:naive_datetime | t], acc),
+    do: decode_datetime(r, null_bitmap, t, acc, :naive_datetime)
+
+  defp decode_binary_row(<<r::bits>>, null_bitmap, [:datetime | t], acc),
+    do: decode_datetime(r, null_bitmap, t, acc, :datetime)
+
+  defp decode_binary_row(<<>>, _null_bitmap, [], acc) do
+    Enum.reverse(acc)
+  end
+
+  defp decode_int1(<<v::int1, r::bits>>, null_bitmap, t, acc),
+    do: decode_binary_row(r, null_bitmap >>> 1, t, [v | acc])
+
+  defp decode_uint1(<<v::uint1, r::bits>>, null_bitmap, t, acc),
+    do: decode_binary_row(r, null_bitmap >>> 1, t, [v | acc])
+
+  defp decode_int2(<<v::int2, r::bits>>, null_bitmap, t, acc),
+    do: decode_binary_row(r, null_bitmap >>> 1, t, [v | acc])
+
+  defp decode_uint2(<<v::uint2, r::bits>>, null_bitmap, t, acc),
+    do: decode_binary_row(r, null_bitmap >>> 1, t, [v | acc])
+
+  defp decode_int4(<<v::int4, r::bits>>, null_bitmap, t, acc),
+    do: decode_binary_row(r, null_bitmap >>> 1, t, [v | acc])
+
+  defp decode_uint4(<<v::uint4, r::bits>>, null_bitmap, t, acc),
+    do: decode_binary_row(r, null_bitmap >>> 1, t, [v | acc])
+
+  defp decode_int8(<<v::int8, r::bits>>, null_bitmap, t, acc),
+    do: decode_binary_row(r, null_bitmap >>> 1, t, [v | acc])
+
+  defp decode_uint8(<<v::uint8, r::bits>>, null_bitmap, t, acc),
+    do: decode_binary_row(r, null_bitmap >>> 1, t, [v | acc])
+
+  defp decode_float(<<v::32-signed-little-float, r::bits>>, null_bitmap, t, acc),
+    do: decode_binary_row(r, null_bitmap >>> 1, t, [v | acc])
+
+  defp decode_double(<<v::64-signed-little-float, r::bits>>, null_bitmap, t, acc),
+    do: decode_binary_row(r, null_bitmap >>> 1, t, [v | acc])
+
+  # in theory it's supposed to be a `string_lenenc` field. However since MySQL decimals
+  # maximum precision is 65 digits, the size of the string will always fir on one byte.
+  defp decode_decimal(<<n::uint1, string::string(n), r::bits>>, null_bitmap, t, acc),
+    do: decode_binary_row(r, null_bitmap >>> 1, t, [Decimal.new(string) | acc])
+
+  defp decode_date(<<4, year::uint2, month::uint1, day::uint1, r::bits>>, null_bitmap, t, acc) do
+    v = %Date{year: year, month: month, day: day}
+    decode_binary_row(r, null_bitmap >>> 1, t, [v | acc])
+  end
+
+  defp decode_time(
+         <<8, 0, 0::uint4, hour::uint1, minute::uint1, second::uint1, r::bits>>,
+         null_bitmap,
+         t,
+         acc
+       ) do
+    v = %Time{hour: hour, minute: minute, second: second}
+    decode_binary_row(r, null_bitmap >>> 1, t, [v | acc])
+  end
+
+  defp decode_time(
+         <<12, 0, 0::uint4, hour::uint1, minute::uint1, second::uint1, microsecond::uint4,
+           r::bits>>,
+         null_bitmap,
+         t,
+         acc
+       ) do
+    v = %Time{hour: hour, minute: minute, second: second, microsecond: {microsecond, 6}}
+    decode_binary_row(r, null_bitmap >>> 1, t, [v | acc])
+  end
+
+  defp decode_time(<<0, r::bits>>, null_bitmap, t, acc) do
+    v = ~T[00:00:00]
+    decode_binary_row(r, null_bitmap >>> 1, t, [v | acc])
+  end
+
+  defp decode_datetime(
+         <<4, year::uint2, month::uint1, day::uint1, r::bits>>,
+         null_bitmap,
+         t,
+         acc,
+         type
+       ) do
+    v = new_datetime(type, year, month, day, 0, 0, 0, {0, 0})
+    decode_binary_row(r, null_bitmap >>> 1, t, [v | acc])
+  end
+
+  defp decode_datetime(
+         <<7, year::uint2, month::uint1, day::uint1, hour::uint1, minute::uint1, second::uint1,
+           r::bits>>,
+         null_bitmap,
+         t,
+         acc,
+         type
+       ) do
+    v = new_datetime(type, year, month, day, hour, minute, second, {0, 0})
+    decode_binary_row(r, null_bitmap >>> 1, t, [v | acc])
+  end
+
+  defp decode_datetime(
+         <<11, year::uint2, month::uint1, day::uint1, hour::uint1, minute::uint1, second::uint1,
+           microsecond::uint4, r::bits>>,
+         null_bitmap,
+         t,
+         acc,
+         type
+       ) do
+    v = new_datetime(type, year, month, day, hour, minute, second, {microsecond, 6})
+    decode_binary_row(r, null_bitmap >>> 1, t, [v | acc])
+  end
+
+  defp new_datetime(:datetime, year, month, day, hour, minute, second, microsecond) do
+    %DateTime{
+      year: year,
+      month: month,
+      day: day,
+      hour: hour,
+      minute: minute,
+      second: second,
+      microsecond: microsecond,
+      std_offset: 0,
+      time_zone: "Etc/UTC",
+      utc_offset: 0,
+      zone_abbr: "UTC"
+    }
+  end
+
+  defp new_datetime(:naive_datetime, year, month, day, hour, minute, second, microsecond) do
+    %NaiveDateTime{
+      year: year,
+      month: month,
+      day: day,
+      hour: hour,
+      minute: minute,
+      second: second,
+      microsecond: microsecond
+    }
+  end
+
+  defp decode_string_lenenc(<<n::int1, v::string(n), r::bits>>, null_bitmap, t, acc) when n < 251,
+    do: decode_binary_row(r, null_bitmap >>> 1, t, [v | acc])
+
+  defp decode_string_lenenc(<<0xFC, n::uint2, v::string(n), r::bits>>, null_bitmap, t, acc),
+    do: decode_binary_row(r, null_bitmap >>> 1, t, [v | acc])
+
+  defp decode_string_lenenc(<<0xFD, n::uint4, v::string(n), r::bits>>, null_bitmap, t, acc),
+    do: decode_binary_row(r, null_bitmap >>> 1, t, [v | acc])
+
+  defp decode_string_lenenc(<<0xFE, n::uint8, v::string(n), r::bits>>, null_bitmap, t, acc),
+    do: decode_binary_row(r, null_bitmap >>> 1, t, [v | acc])
+
+  defp decode_json(<<n::int1, v::string(n), r::bits>>, null_bitmap, t, acc) when n < 251,
+    do: decode_binary_row(r, null_bitmap >>> 1, t, [decode_json(v) | acc])
+
+  defp decode_json(<<0xFC, n::uint2, v::string(n), r::bits>>, null_bitmap, t, acc),
+    do: decode_binary_row(r, null_bitmap >>> 1, t, [decode_json(v) | acc])
+
+  defp decode_json(<<0xFD, n::uint4, v::string(n), r::bits>>, null_bitmap, t, acc),
+    do: decode_binary_row(r, null_bitmap >>> 1, t, [decode_json(v) | acc])
+
+  defp decode_json(<<0xFE, n::uint8, v::string(n), r::bits>>, null_bitmap, t, acc),
+    do: decode_binary_row(r, null_bitmap >>> 1, t, [decode_json(v) | acc])
+
+  defp decode_json(string), do: MyXQL.json_library().decode!(string)
 end
