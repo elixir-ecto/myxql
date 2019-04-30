4b902d8f283785190429cb3a03471c67178db501
Wojtek Mach <wojtek@wojtekmach.pl>
2019-04-30 12:19:37 +0200

    Refactor TestHelper

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,21 +1,11 @@
 defmodule TestHelper do
   def opts() do
-    ssl_opts =
-      case System.get_env("FORCE_TLS11") do
-        "true" ->
-          [versions: [:"tlsv1.1"]]
-
-        nil ->
-          []
-      end
-
     [
       hostname: "127.0.0.1",
       username: "root",
       database: "myxql_test",
       timeout: 5000,
-      ssl: false,
-      ssl_opts: ssl_opts,
+      ssl_opts: ssl_opts(),
       backoff_type: :stop,
       max_restarts: 0,
       pool_size: 1,
@@ -23,172 +13,206 @@ defmodule TestHelper do
     ]
   end
 
+  defp ssl_opts() do
+    if System.get_env("FORCE_TLS11") == "true", do: [versions: [:"tlsv1.1"]], else: []
+  end
+
+  def setup_server() do
+    configure_server()
+    create_test_database()
+    create_test_users()
+    create_test_tables()
+  end
+
+  def configure_server() do
+    mysql("""
+    -- set packet size to 100mb
+    SET GLOBAL max_allowed_packet=#{100_000_000};
+    """)
+  end
+
+  def create_test_database() do
+    mysql("""
+    DROP DATABASE IF EXISTS myxql_test;
+    CREATE DATABASE myxql_test;
+    """)
+  end
+
+  def create_test_users() do
+    mysql("""
+    DROP USER IF EXISTS default_auth;
+    CREATE USER default_auth IDENTIFIED BY 'secret';
+    GRANT ALL PRIVILEGES ON myxql_test.* TO default_auth;
+
+    DROP USER IF EXISTS mysql_native_password;
+    CREATE USER mysql_native_password IDENTIFIED WITH mysql_native_password;
+    ALTER USER mysql_native_password IDENTIFIED BY 'secret';
+    GRANT ALL PRIVILEGES ON myxql_test.* TO mysql_native_password;
+
+    DROP USER IF EXISTS nopassword;
+    CREATE USER nopassword;
+    GRANT ALL PRIVILEGES ON myxql_test.* TO nopassword;
+    """)
+
+    auth_plugins = auth_plugins()
+
+    if "sha256_password" in auth_plugins do
+      mysql("""
+      DROP USER IF EXISTS sha256_password;
+      CREATE USER sha256_password IDENTIFIED WITH sha256_password;
+      ALTER USER sha256_password IDENTIFIED BY 'secret';
+      GRANT ALL PRIVILEGES ON myxql_test.* TO sha256_password;
+      """)
+    end
+
+    if "caching_sha2_password" in auth_plugins do
+      mysql("""
+      DROP USER IF EXISTS caching_sha2_password;
+      CREATE USER caching_sha2_password IDENTIFIED WITH caching_sha2_password;
+      ALTER USER caching_sha2_password IDENTIFIED BY 'secret';
+      GRANT ALL PRIVILEGES ON myxql_test.* TO caching_sha2_password;
+      """)
+    end
+  end
+
+  def create_test_tables() do
+    mysql("""
+    USE myxql_test;
+
+    CREATE TABLE integers (x int);
+
+    CREATE TABLE uniques (a int UNIQUE);
+
+    CREATE TABLE test_types (
+      id SERIAL PRIMARY KEY,
+      my_tinyint TINYINT,
+      my_smallint SMALLINT,
+      my_mediumint MEDIUMINT,
+      my_int INT,
+      my_bigint BIGINT,
+      my_float FLOAT,
+      my_double DOUBLE,
+      my_decimal DECIMAL, /* same as: DECIMAL(10, 0) */
+      my_decimal52 DECIMAL(5,2),
+
+      my_unsigned_tinyint TINYINT UNSIGNED,
+      my_unsigned_smallint SMALLINT UNSIGNED,
+      my_unsigned_mediumint MEDIUMINT UNSIGNED,
+      my_unsigned_int INT UNSIGNED,
+      my_unsigned_bigint BIGINT UNSIGNED,
+      my_unsigned_float FLOAT UNSIGNED,
+      my_unsigned_double DOUBLE UNSIGNED,
+      my_unsigned_decimal DECIMAL UNSIGNED,
+      my_unsigned_decimal52 DECIMAL(5, 2) UNSIGNED,
+
+      my_enum ENUM('red', 'green', 'blue'),
+      my_set SET('red', 'green', 'blue'),
+      my_bit2 BIT(2),
+      my_bit8 BIT(8),
+      my_date DATE,
+      my_time TIME,
+      my_time3 TIME(3),
+      my_time6 TIME(6),
+      my_datetime DATETIME,
+      my_datetime3 DATETIME(3),
+      my_datetime6 DATETIME(6),
+      my_timestamp TIMESTAMP,
+      my_year YEAR,
+      my_binary3 BINARY(3),
+      my_varbinary3 VARBINARY(3),
+      my_boolean BOOLEAN,
+      my_blob BLOB,
+      my_mediumblob MEDIUMBLOB,
+      my_json JSON,
+      my_char CHAR
+    );
+
+    DROP PROCEDURE IF EXISTS single_procedure;
+    DELIMITER $$
+    CREATE PROCEDURE single_procedure()
+    BEGIN
+      SELECT 1;
+    END$$
+    DELIMITER ;
+
+    DROP PROCEDURE IF EXISTS multi_procedure;
+    DELIMITER $$
+    CREATE PROCEDURE multi_procedure()
+    BEGIN
+      SELECT 1;
+      SELECT 2;
+    END$$
+    DELIMITER ;
+    """)
+  end
+
   def auth_plugins() do
-    {:ok, pid} = MyXQL.start_link(opts())
-
-    %{rows: plugins} =
-      MyXQL.query!(
-        pid,
-        "SELECT plugin_name FROM information_schema.plugins WHERE plugin_type = 'authentication'"
-      )
-
-    List.flatten(plugins)
+    "SELECT plugin_name FROM information_schema.plugins WHERE plugin_type = 'authentication'"
+    |> mysql()
+    |> String.split("\n", trim: true)
+    |> Enum.drop(1)
   end
 
   def default_auth_plugin() do
-    {:ok, pid} = MyXQL.start_link(opts())
-
-    %MyXQL.Result{rows: [[plugin_name]]} =
-      MyXQL.query!(pid, "SELECT plugin FROM mysql.user WHERE user = 'root' LIMIT 1")
-
-    plugin_name
+    "SELECT @@default_authentication_plugin"
+    |> mysql()
+    |> String.split("\n", trim: true)
+    |> Enum.at(1)
   end
 
-  def mysql(sql) do
+  def mysql(sql, options \\ []) do
     args = ~w(
       --protocol=tcp
       --user=root
     ) ++ ["-e", sql]
 
-    cmd(["mysql" | args])
+    cmd(["mysql" | args], options)
   end
 
-  def cmd([command | args]) do
-    case System.cmd(command, args) do
-      {"", 0} ->
-        :ok
-
+  def cmd([command | args], options) do
+    case System.cmd(command, args, options) do
       {result, 0} ->
-        IO.puts(result)
+        result
 
       {result, _} ->
         IO.puts(result)
         exit(:error)
     end
   end
-end
 
-TestHelper.mysql("""
-DROP DATABASE IF EXISTS myxql_test;
-CREATE DATABASE myxql_test;
-""")
+  def excludes() do
+    exclude = if System.otp_release() >= "19", do: [], else: [:requires_otp_19]
+    auth_plugins = auth_plugins()
 
-sha256_password_available? = "sha256_password" in TestHelper.auth_plugins()
+    exclude =
+      if "sha256_password" in auth_plugins do
+        exclude
+      else
+        [{:auth_plugin, "sha256_password"} | exclude]
+      end
 
-if sha256_password_available? do
-  TestHelper.mysql("""
-  DROP USER IF EXISTS sha256_password;
-  CREATE USER sha256_password IDENTIFIED WITH sha256_password;
-  ALTER USER sha256_password IDENTIFIED BY 'secret';
-  GRANT ALL PRIVILEGES ON myxql_test.* TO sha256_password;
-  """)
-end
+    exclude =
+      if "caching_sha2_password" in auth_plugins do
+        exclude
+      else
+        [{:auth_plugin, "caching_sha2_password"} | exclude]
+      end
 
-# set packet size to 100mb
-TestHelper.mysql("""
-set global max_allowed_packet=100000000;
-""")
+    exclude =
+      case System.get_env("JSON") do
+        "false" -> [{:requires_json, true} | exclude]
+        _ -> exclude
+      end
 
-TestHelper.mysql("""
-USE myxql_test;
-DROP USER IF EXISTS default_auth;
-CREATE USER default_auth IDENTIFIED BY 'secret';
-GRANT ALL PRIVILEGES ON myxql_test.* TO default_auth;
+    exclude =
+      case System.get_env("SSL") do
+        "false" -> [{:requires_ssl, true} | exclude]
+        _ -> exclude
+      end
 
-DROP USER IF EXISTS mysql_native_password;
-CREATE USER mysql_native_password IDENTIFIED WITH mysql_native_password;
-ALTER USER mysql_native_password IDENTIFIED BY 'secret';
-GRANT ALL PRIVILEGES ON myxql_test.* TO mysql_native_password;
-
-DROP USER IF EXISTS nopassword;
-CREATE USER nopassword;
-GRANT ALL PRIVILEGES ON myxql_test.* TO nopassword;
-
-CREATE TABLE integers (x int);
-
-CREATE TABLE uniques (a int UNIQUE);
-
-CREATE TABLE test_types (
-  id SERIAL PRIMARY KEY,
-  my_tinyint TINYINT,
-  my_smallint SMALLINT,
-  my_mediumint MEDIUMINT,
-  my_int INT,
-  my_bigint BIGINT,
-  my_float FLOAT,
-  my_double DOUBLE,
-  my_decimal DECIMAL, /* same as: DECIMAL(10, 0) */
-  my_decimal52 DECIMAL(5,2),
-
-  my_unsigned_tinyint TINYINT UNSIGNED,
-  my_unsigned_smallint SMALLINT UNSIGNED,
-  my_unsigned_mediumint MEDIUMINT UNSIGNED,
-  my_unsigned_int INT UNSIGNED,
-  my_unsigned_bigint BIGINT UNSIGNED,
-  my_unsigned_float FLOAT UNSIGNED,
-  my_unsigned_double DOUBLE UNSIGNED,
-  my_unsigned_decimal DECIMAL UNSIGNED,
-  my_unsigned_decimal52 DECIMAL(5, 2) UNSIGNED,
-
-  my_enum ENUM('red', 'green', 'blue'),
-  my_set SET('red', 'green', 'blue'),
-  my_bit2 BIT(2),
-  my_bit8 BIT(8),
-  my_date DATE,
-  my_time TIME,
-  my_time3 TIME(3),
-  my_time6 TIME(6),
-  my_datetime DATETIME,
-  my_datetime3 DATETIME(3),
-  my_datetime6 DATETIME(6),
-  my_timestamp TIMESTAMP,
-  my_year YEAR,
-  my_binary3 BINARY(3),
-  my_varbinary3 VARBINARY(3),
-  my_boolean BOOLEAN,
-  my_blob BLOB,
-  my_mediumblob MEDIUMBLOB,
-  my_json JSON,
-  my_char CHAR
-);
-
-DROP PROCEDURE IF EXISTS single_procedure;
-DELIMITER $$
-CREATE PROCEDURE single_procedure()
-BEGIN
-  SELECT 1;
-END$$
-DELIMITER ;
-
-DROP PROCEDURE IF EXISTS multi_procedure;
-DELIMITER $$
-CREATE PROCEDURE multi_procedure()
-BEGIN
-  SELECT 1;
-  SELECT 2;
-END$$
-DELIMITER ;
-""")
-
-exclude = if System.otp_release() >= "19", do: [], else: [:requires_otp_19]
-
-exclude =
-  if sha256_password_available? do
     exclude
-  else
-    [{:auth_plugin, "sha256_password"} | exclude]
   end
+end
 
-exclude =
-  case System.get_env("JSON") do
-    "false" -> [{:requires_json, true} | exclude]
-    _ -> exclude
-  end
-
-exclude =
-  case System.get_env("SSL") do
-    "false" -> [{:requires_ssl, true} | exclude]
-    _ -> exclude
-  end
-
-ExUnit.start(exclude: exclude)
+TestHelper.setup_server()
+ExUnit.start(exclude: TestHelper.excludes())
