c36a9a0baafe0c78b1cead40f699ab1728eb6e74
Wojtek Mach <wojtek@wojtekmach.pl>
2019-05-06 10:57:01 +0200

    Add docker-compose.yml and scripts/test-versions for local testing

---
+++ scripts/test-versions.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# Usage:
+#   ./test.sh [-c COMMAND] [NAME1 [NAME2 [...]]]
+
+if [[ "$1" == "-c" ]]; then
+  cmd="$2"
+  shift
+  shift
+else
+  cmd="mix test"
+fi
+
+if [[ "$@" == "" ]]; then
+  services=`docker-compose config --services | xargs echo`
+else
+  services="$@"
+fi
+
+for name in $services; do
+  port=`docker inspect --format='{{(index (index .NetworkSettings.Ports "3306/tcp") 0).HostPort}}' myxql_${name}_1`
+  echo $name
+  MYSQL_TCP_PORT=$port $cmd
+done
