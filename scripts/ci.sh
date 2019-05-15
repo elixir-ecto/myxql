bd59eccaf746c5d6cf5212380c3373a9aa7b11f7
Wojtek Mach <wojtek@wojtekmach.pl>
2019-05-15 18:06:16 +0200

    Backport 614801e to ci.sh

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -12,7 +12,7 @@ sudo chmod 777 $MYSQL_SOCKET_DIR
 
 docker pull $DB || true
 docker rm --force $name || true
-docker run --name $name -p $MYSQL_TCP_PORT:3306 --volume $MYSQL_SOCKET_DIR:/var/run/mysqld -e MYSQL_ALLOW_EMPTY_PASSWORD=1 -d $DB
+docker run --name $name -p $MYSQL_TCP_PORT:3306 --volume $MYSQL_SOCKET_DIR:/var/run/mysqld -e MYSQL_ALLOW_EMPTY_PASSWORD=1 -d $DB --innodb_log_file_size=1G
 mysql --version
 until mysql -u root --protocol=tcp -e "SELECT @@version;"; do sleep 1; done
 mix test $1
