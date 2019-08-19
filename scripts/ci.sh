665380d78f082b03f6d9f880f1dc331d7e3a9c72
Wojtek Mach <wojtek@wojtekmach.pl>
2019-08-19 21:45:37 +0200

    Fix build

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -7,11 +7,11 @@ set -x
 : "${MYSQL_SOCKET_DIR:=/tmp/mysqld}"
 name="myxql-ci"
 
-mkdir -p $MYSQL_SOCKET_DIR
+sudo mkdir -p $MYSQL_SOCKET_DIR
 sudo chmod 777 $MYSQL_SOCKET_DIR
 
 docker pull $DB || true
-docker rm --force $name || true
+# docker rm --force $name || true
 docker run --name $name -p $MYSQL_TCP_PORT:3306 --volume $MYSQL_SOCKET_DIR:/var/run/mysqld -e MYSQL_ALLOW_EMPTY_PASSWORD=1 -d $DB --innodb_log_file_size=1G
 mysql --version
 until mysql -u root --protocol=tcp -e "SELECT @@version;"; do sleep 1; done
