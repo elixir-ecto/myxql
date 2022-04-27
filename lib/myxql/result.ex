223f104e26c0e5f474ebb2e8f78a64bf7b023df8
Jonatan Kłosko <jonatanklosko@gmail.com>
2022-04-27 15:40:12 +0200

    Implement the Table.Reader protocol for query result (#154)

--- lib/myxql/result.ex
+++ lib/myxql/result.ex
@@ -40,3 +40,15 @@ defmodule MyXQL.Result do
     :num_warnings
   ]
 end
+
+if Code.ensure_loaded?(Table.Reader) do
+  defimpl Table.Reader, for: MyXQL.Result do
+    def init(%{columns: columns}) when columns in [nil, []] do
+      {:rows, %{columns: []}, []}
+    end
+
+    def init(result) do
+      {:rows, %{columns: result.columns}, result.rows}
+    end
+  end
+end
