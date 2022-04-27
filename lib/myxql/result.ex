59536d39ba181bd72c8fe25f39eb37861a84ac7c
José Valim <jose.valim@gmail.com>
2022-04-27 15:32:57 +0200

    Update lib/myxql/result.ex

--- lib/myxql/result.ex
+++ lib/myxql/result.ex
@@ -43,7 +43,7 @@ end
 
 if Code.ensure_loaded?(Table.Reader) do
   defimpl Table.Reader, for: MyXQL.Result do
-    def init(result) when result.columns in [nil, []] do
+    def init(%{columns: columns} = result) when columns in [nil, []] do
       {:rows, %{columns: []}, []}
     end
 
