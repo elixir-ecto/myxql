83cf54d0df81460646ffe6686eeea420e125b3db
Christian Koch <54853932+ckoch-cars@users.noreply.github.com>
2024-10-10 09:28:56 -0500

    permit geo ~> V4.0

--- mix.exs
+++ mix.exs
@@ -48,7 +48,7 @@ defmodule MyXQL.MixProject do
       {:db_connection, "~> 2.4.1 or ~> 2.5", db_connection_opts()},
       {:decimal, "~> 1.6 or ~> 2.0"},
       {:jason, "~> 1.0", optional: true},
-      {:geo, "~> 3.4", optional: true},
+      {:geo, "~> 3.4 or ~> 4.0", optional: true},
       {:table, "~> 0.1.0", optional: true},
       {:binpp, ">= 0.0.0", only: [:dev, :test]},
       {:dialyxir, "~> 1.0", only: :dev, runtime: false},
