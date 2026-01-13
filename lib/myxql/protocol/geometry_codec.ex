0cf16d04642ce1c482b8b921666f2d2b59436a19
Aaron Seigo <aseigo@mykolab.com>
2026-01-13 13:41:54 +0100

    Apply suggestions from code review
    Co-authored-by: Wojtek Mach <wojtekmach@users.noreply.github.com>

--- lib/myxql/protocol/geometry_codec.ex
+++ lib/myxql/protocol/geometry_codec.ex
@@ -1,9 +1,9 @@
 defmodule MyXQL.Protocol.GeometryCodec do
-  @type no_srid :: 0
-  @type some_srid :: pos_integer
+  @type no_srid() :: 0
+  @type some_srid() :: pos_integer()
 
-  @callback encode(struct) :: {srid :: integer, wkb :: binary()} | :unknown
-  @callback decode(srid :: no_srid | some_srid, wkb :: binary) :: struct() | :unknown
+  @callback encode(struct()) :: {srid :: integer(), wkb :: binary()} | :unknown
+  @callback decode(srid :: no_srid() | some_srid(), wkb :: binary()) :: struct() | :unknown
 
   import MyXQL.Protocol.Types
 
@@ -33,7 +33,7 @@ defmodule MyXQL.Protocol.GeometryCodec do
 
   default_codec = if Code.ensure_loaded?(Geo), do: MyXQL.Protocol.GeometryCodec.Geo, else: nil
 
-  defp(geometry_codec) do
+  defp geometry_codec do
     Application.get_env(:myxql, :geometry_codec, unquote(default_codec))
   end
 end
